79be3c67d657688b296674395dcace144dd05575
Gilyana Bachaeva <gelchik29@icloud.com>
2026-01-09 23:20:42 +0300

    feat: добавить аутентификацию OAuth

---
+++ api-gateway-oauth/src/main/java/fileconverter/apigateway/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package fileconverter.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {})
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
